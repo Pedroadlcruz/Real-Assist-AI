ef951f4ae9586a012f93d0bd0cdc02958f941ae5
Pedro A. De la Cruz <pedrodlcruz@coolhunterpartners.com>
2023-03-22 17:01:36 -0400

    ✨ Set up firebase

--- real_assit_ai/lib/main.dart
+++ real_assit_ai/lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:real_assit_ai/real_assist_app.dart';
 
+import 'firebase_options.dart';
+
 void main() async {
   await setupAppSystemPreferrence();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await dotenv.load(fileName: ".env");
   runApp(const RealAssistApp());
 }
